ba5b691bb37c206815e985eff539a638252a6266
yiz <ql3895619@gmail.com>
2019-10-04 10:39:02 +0800

    Feat validate with cross validation

--- finetune_sigunet.py
+++ finetune_sigunet.py
@@ -3,18 +3,14 @@ from bert.train.train_sigunet import finetuneSigunet
 import torch
 import datetime
 
-model_name='test_fixed'
 pretrained_checkpoint = 'models/pretrain/SignalP_euk/checkpoint'
 data_dir = None
-train_path = 'data/finetune_features/SignalP/SignalP_euk_train.txt'
-val_path = 'data/finetune_features/SignalP/SignalP_euk_val.txt'
 dictionary_path = 'dic/dic.txt'
-checkpoint_dir = f'models/finetune/{model_name}'
 dataset_limit = None
-epochs = 200
+epochs = 300
 batch_size = 128
 print_every = 1
-save_every = 10
+save_every = 5
 vocabulary_size = 30000
 max_len = 1024
 lr = 0.0001
@@ -27,13 +23,26 @@ dropout_prob = 0.1
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 config = None
 pretrain_fixed=True
-run_name = f"Sigunet:{layers_count}-hidden_size:{hidden_size}-heads_count:{heads_count}-fixed:{pretrain_fixed}-timestamp:{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-log_output = 'log/%s.log' % run_name
 
 if __name__ == '__main__':
-    finetuneSigunet(pretrained_checkpoint,\
-            data_dir, train_path, val_path, dictionary_path,\
-            vocabulary_size, batch_size, max_len, epochs,\
-            lr, clip_grads, device, layers_count, hidden_size, heads_count,\
-            d_ff, dropout_prob, log_output, checkpoint_dir, print_every,\
-            save_every, config, model_name, pretrain_fixed=pretrain_fixed)
+
+    best_models = []
+
+    for i in range(5):
+        model_name = f'Sigunet_msk15_{i}'
+        run_name = f"{model_name}:{layers_count}-hidden_size:{hidden_size}-heads_count:{heads_count}-fixed:{pretrain_fixed}-timestamp:{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        train_path = f'data/finetune_features/SignalP/euk_train_{i}.txt'
+        val_path = f'data/finetune_features/SignalP/euk_valid_{i}.txt'
+        checkpoint_dir = f'models/finetune/{run_name}'
+        log_output = f'log/{run_name}.log'
+
+        trainer = finetuneSigunet(pretrained_checkpoint,\
+                data_dir, train_path, val_path, dictionary_path,\
+                vocabulary_size, batch_size, max_len, epochs,\
+                lr, clip_grads, device, layers_count, hidden_size, heads_count,\
+                d_ff, dropout_prob, log_output, checkpoint_dir, print_every,\
+                save_every, config, model_name, pretrain_fixed=pretrain_fixed)
+
+        best_models.append(trainer.best_checkpoint_output_path)
+
+    print(best_models)
